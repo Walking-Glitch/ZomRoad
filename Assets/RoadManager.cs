4abd41e579e3de34cfd4f977684dd6413f00cf80
Walking-Glitch <84610569+Walking-Glitch@users.noreply.github.com>
2024-04-02 00:05:23 -0400

    Procedural level 1.0
    + Added street manager to add and delete first streets
    + Added streets assets to project

---
+++ Assets/RoadManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour
+{
+    public GameObject currStreet;
+    private GameObject prevStreet;
+    [SerializeField] private GameObject[] streets;
+    [SerializeField] private Transform streetEndPoint;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.gameObject.CompareTag("RoadTrigger"))
+        {
+            streetEndPoint = currStreet.transform.Find("CreatePoint");
+
+            if (streetEndPoint != null)
+            {
+                GameObject tempStreetPrefab = Instantiate(streets[Random.Range(0,streets.Length)], streetEndPoint.position + new Vector3(0, 0, 30), Quaternion.identity);
+                prevStreet = currStreet;
+                currStreet = tempStreetPrefab;
+                
+            }
+           
+
+        }
+
+        if (other.gameObject.CompareTag("DeleteTrigger"))
+        {
+            Destroy(prevStreet);
+        }
+    }
+
+
+}
