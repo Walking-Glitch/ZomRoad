322d285178018e67346814dccafb9ada6478349c
Walking-Glitch <84610569+Walking-Glitch@users.noreply.github.com>
2024-04-16 16:18:36 -0400

    UI implementation 1.0
    + Health and XP system for player added
    + UI slidebars linked

---
+++ Assets/UIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Slider xpSlider;
+
+    public void SetMaxHealth(int maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+    }
+
+    public void SetHealth(int health)
+    {
+        healthSlider.value = health;
+    }
+
+    public void SetMaxXp(int maxXp)
+    {
+        xpSlider.maxValue = maxXp;
+        xpSlider.value = 0;
+    }
+
+    public void SetXp(int xp)
+    {
+        xpSlider.value = xp;
+    }
+}
